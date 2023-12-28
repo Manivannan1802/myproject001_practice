ae097a2238424bc93da16dfb5f00edc18006c8e4
Megala <megalachandrasekar11@gmail.com>
2023-12-28 15:08:28 +0530

    Feature file folder created

--- src/test/java/testRun/testRunner.java
+++ src/test/java/testRun/testRunner.java
@@ -15,7 +15,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions
 (
-		features="C:/Users/sanma/eclipse-workspace/myproject001_practice/src/test/java/feature/login.feature",
+		features="C:\\Users\\megal\\git\\myproject001_practice\\src\\test\\java\\feature\\login.feature",
 		glue={"stepDefinitions"},
 		dryRun=false,
 		monochrome=true,// display console output in a proper readable format
